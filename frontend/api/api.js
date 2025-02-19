225c9cd615eb5a9db0c37709ca1c41f104722c2e
Gabriely Xavier <xgabriely00@gmail.com>
2025-02-19 20:48:21 -0300

    ajuste no process env

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -1,8 +1,9 @@
 import "dotenv/config";
 import axios from "axios"; // Importa a biblioteca Axios para fazer requisições HTTP
 
-const {NODE_ENV}= process.env;
-const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api"
+// const {NODE_ENV}= process.env;
+// const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api"
+const URL = "http://spotify-fvn5.onrender.com/api";
 // // Define a URL base da API
 
 // Faz uma requisição GET para obter os dados de artistas do backend
