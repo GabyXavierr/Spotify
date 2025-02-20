21386b1adc9851997cd792f84d88085bac8a969f
Gabriely Xavier <xgabriely00@gmail.com>
2025-02-19 21:07:48 -0300

    ajustes na api

--- frontend/api/api.js
+++ frontend/api/api.js
@@ -3,7 +3,7 @@ import axios from "axios"; // Importa a biblioteca Axios para fazer requisiçõe
 
 // const {NODE_ENV}= process.env;
 // const URL = NODE_ENV === "development" ? "http://localhost:3000/api" : "/api"
-const URL = "http://spotify-fvn5.onrender.com/api";
+const URL = "https://spotify-fvn5.onrender.com/api";
 // // Define a URL base da API
 
 // Faz uma requisição GET para obter os dados de artistas do backend
